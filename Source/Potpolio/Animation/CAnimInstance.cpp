8b956698f13093e95622f3e278a1afc073ce1883
hiki25 <50028225+hiki25@users.noreply.github.com>
2024-07-22 04:43:29 +0900

    0722
    무기 상태 변경 시 포즈 변경

--- Source/Potpolio/Animation/CAnimInstance.cpp
+++ Source/Potpolio/Animation/CAnimInstance.cpp
@@ -1,18 +1,46 @@
 #include "CAnimInstance.h"
+#include "Global.h"
 #include "Kismet/GameplayStatics.h"
 #include "GameFramework/Character.h"
 
+void UCAnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	CheckNull(TryGetPawnOwner());
+
+	UCActionComponent* ActionComp = CHelpers::GetComponent<UCActionComponent>(TryGetPawnOwner());
+
+	CheckNull(ActionComp);
+
+	ActionComp->OnActionTypeChanged.AddDynamic(this, &UCAnimInstance::OnActionTypeChanged);
+}
 
 void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
+	Super::NativeUpdateAnimation(DeltaSeconds);
 	//Save Player
 	Player = Cast<ACharacter>(TryGetPawnOwner());
-	if (Player == nullptr)
-	{
-		return;
-	}
+	CheckNull(Player);
 
+	
 
 //Save Speed
 	Speed = Player->GetVelocity().Size2D();
+
+	Jump = Player->GetVelocity().Z;
+
+	if (Jump <= 0)
+	{
+		bFalling = false;
+	}
+	else
+	{
+		bFalling = true;
+	}
+}
+
+void UCAnimInstance::OnActionTypeChanged(EActionType InPrevType, EActionType InNewType)
+{
+	ActionType = InNewType;
 }
--- Source/Potpolio/Animation/CAnimInstance.h
+++ Source/Potpolio/Animation/CAnimInstance.h
@@ -2,24 +2,35 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "Component/CActionComponent.h"
 #include "CAnimInstance.generated.h"
 
 class ACharacter;
+class UCActionComponent;
 
 UCLASS()
 class POTPOLIO_API UCAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 
-
-public:
-	//UpdateAnimation
+protected:
+	virtual void NativeBeginPlay() override;
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UFUNCTION()
+		void OnActionTypeChanged(EActionType InPrevType, EActionType InNewType);
 
 protected:
 	ACharacter* Player;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Blend")
 	float Speed;
-	
+	UPROPERTY(BlueprintReadOnly, Category = "Blend")
+	bool bFalling;
+	UPROPERTY(BlueprintReadOnly, Category = "Blend")
+	float Jump;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "ActionType")
+		EActionType ActionType;
 };
